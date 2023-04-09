812d850c1a6429e623e65e655e203a500551e56a
Syaz <sitisyazielawati@gmail.com>
2023-04-09 12:45:22 +0800

    created bird variables and canvas

--- flappybird.js
+++ flappybird.js
@@ -4,9 +4,31 @@ let boardWidth = 360;
 let boardHeight = 640;
 let context; // setting a variable
 
+// bird
+let birdWidth = 34; 
+let birdHeight = 24;
+
+// to get the bird spawning a certain location of the board
+let birdX = boardWidth/8; // x-axis (1/8)
+let birdY = boardHeight/2; // y-axis (1/2)
+let birdImg;
+
+// bird object
+let bird = {
+    x : birdX,
+    y : birdY,
+    width : birdWidth,
+    height : birdHeight,
+}
+
 window.onload = function() { // upon window loaded
     board = document.getElementById("board"); // getting DOM element and placing it under a variable.
     board.height = boardHeight; // setting the height to the inital, 640.
     board.width = boardWidth; //setting the width to the initial, 360.
     context = board.getContext("2d"); // used for drawing on the board.
+
+    // draw flappy bird
+    context.fillStyle = "green"; // similar to background-color 
+    context.fillRect(bird.x, bird.y, bird.width, bird.height); //fills up green to the respective keys of the bird object
+
 }
